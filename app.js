d58c28488aec9262fd92fc17152b2ab79e799f9e
sanyog2005 <20sanyogsharma.com>
2025-09-26 15:11:00 +0530

    df# with '#' will be ignored, and an empty message aborts the commit.

---
+++ app.js
@@ -0,0 +1,8 @@
+const a =()=>{
+    console.log("hello");
+    
+}
+const a = require('fs');
+const math = require('./math');
+console.log(a());
+console.log(math.add(4,5));
---
+++ file.js
@@ -0,0 +1,4 @@
+const { log } = require('console');
+const path = require('path');
+const fss = path.join("ssdf","bsdf","c.txt");
+console.log(fss);
